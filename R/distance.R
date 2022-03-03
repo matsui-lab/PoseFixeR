cd407d2b36e6ee4efd6c29f1bd6462949df91d3a
Yusuke Matsui <mail.to.matsui@gmail.com>
2022-03-03 14:25:30 +0900

    Delete R directory

--- R/distance.R
+++
@@ -1,6 +0,0 @@
-distance = function(dat,a,b,time){
-  x = dat$x[time,a] - dat$x[time,b]
-  y = dat$y[time,a] - dat$y[time,b]
-  dist = sqrt(x^2 + y^2) 
-  return(dist)
-}
--- R/hazureti.R
+++
@@ -1,6 +0,0 @@
-hazureti<- function(x, k=2) {
-  result <- NULL
-  result$caselist <- x >= mean(x)+k*sd(x) | x <= mean(x)-k*sd(x)
-  result$abnormal.values <- sort(x[result$caselist])
-  result
-}
