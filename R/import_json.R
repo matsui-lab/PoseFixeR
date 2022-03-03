a7a8219cc83e4d52d85097b0dc428c85b7294080
ymatts <mail.to.matsui@gmail.com>
2022-03-03 17:54:27 +0900

    updated

--- R/import_json.R
+++ R/import_json.R
@@ -5,14 +5,14 @@
 #'
 #' @export
 #'
-import_json = function(myfiles,parts =
+import_json = function(files,parts =
                          c("nose","neck","Rshoulder","Relbow","Rwrist","Lshoulder",
                                          "Lelbow","Lwrist","Rhip","Rknee","Rankle","Lhip",
                                          "Lknee","Lankle","Reye","Leye","Rear","Lear")){
-  n_file=length(myfiles)
+  n_file = length(files)
   f_df = vector("list",n_file)
   for(i in 1:n_file){
-    output = rjson::fromJSON(file = myfiles[i])
+    output = rjson::fromJSON(file = files[i])
     f_df[[i]] = matrix(output,nrow=18,ncol=3,byrow=T)
     rownames(f_df[[i]]) = parts
     colnames(f_df[[i]])=c("X","Y","P")
